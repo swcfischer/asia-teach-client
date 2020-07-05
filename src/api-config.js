70aa0af93d1fac168e098cbe054c9baf945149d1
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-05 08:02:19 -0500

    changed api call, again

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendPath = '';
 } else {
-  backendPath = '';
+  backendPath = 'https://historic-arches-33577.herokuapp.com';
 }
 
 export const API_ROOT = backendPath;
