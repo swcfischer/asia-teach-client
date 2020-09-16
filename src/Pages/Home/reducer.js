d71347ffba2fd2f2cfe111bc3efce764e2c09e57
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-09-16 11:05:07 -0500

    mobile view

--- src/Pages/Home/reducer.js
+++ src/Pages/Home/reducer.js
@@ -1,15 +1,6 @@
-// const EXAMPLE_TYPE = 'EXAMPLE_TYPE';
-
-// export function handleSearch(name = 'location', value) {
-//   return {
-//     type: HANDLE_SEARCH,
-//     payload: { name, value }
-//   };
-// }
-
 const initialState = {};
 
-export default function(state = initialState, { type, payload }) {
+export default function (state = initialState, { type, payload }) {
   switch (type) {
     default:
       return state;
