45a2488ff37619bbeccf87e25a9089b17b31ac7b
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-01-18 10:06:07 -0600

    handling no photo for result items

--- src/Pages/Search/components/Results/ResultItem.jsx
+++ src/Pages/Search/components/Results/ResultItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
@@ -47,6 +47,23 @@ export const durationOptions = [
   { label: 'More than 1 year', value: 13 },
 ];
 
+const mapCountryToStockPhoto = {
+  thailand:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/thailand.png',
+  china:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/china.png',
+  vietnam:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/vietnam.png',
+  indonesia:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/indonesia.png',
+  'south-korea':
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/korea.png',
+  japan:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/japan.png',
+  taiwan:
+    'https://static-files-asia-teach.s3.amazonaws.com/example_images/taiwan.png',
+};
+
 export default function ResultItem(props) {
   const {
     thumbnail,
@@ -65,6 +82,24 @@ export default function ResultItem(props) {
   labelDate = startDateOptions.find((el) => el.value === startDate)?.label;
   let labelDuration = '';
   labelDuration = durationOptions.find((el) => el.value === duration)?.label;
+
+  const handlePhoto = useCallback(() => {
+    // For now, this will not handle the local static file
+    if (!thumbnail) {
+      return mapCountryToStockPhoto[country];
+    } else {
+      return thumbnail;
+    }
+
+    /* Previous logic
+        thumbnail && thumbnail.includes('https')
+          ? thumbnail
+          : thumbnail
+          ? `/assets/${thumbnail}`
+          : 'https://static-files-asia-teach.s3.amazonaws.com/korea-bg-2.jpg'
+    */
+  }, [country, thumbnail]);
+
   return (
     <Link to={`/posting/${uuid}`} className="posting-link">
       <div className="result-item">
@@ -73,15 +108,7 @@ export default function ResultItem(props) {
           <div className="thumbnail-container-width">
             <div className="thumbnail-container">
               {/* Will want to add more permanent conditional logic */}
-              <img
-                src={
-                  thumbnail && thumbnail.includes('https')
-                    ? thumbnail
-                    : `/assets/${thumbnail}`
-                }
-                loading="lazy"
-                alt="school thumbnail"
-              />
+              <img src={handlePhoto()} loading="lazy" alt="school thumbnail" />
             </div>
           </div>
           <div className="bottom-section">
