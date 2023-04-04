683f4aa9cc10120dbf24c6e02e9b2e0fa4bb692d
Steven Fischer <swcfischer@gmail.com>
2023-04-04 13:22:53 -0500

    changes

--- src/api-config.js
+++ src/api-config.js
@@ -2,10 +2,10 @@ let backendPath;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'localhost') {
-  backendPath = 'http://localhost:5000';
+if (hostname === "localhost") {
+  backendPath = "http://localhost:8888";
 } else {
-  backendPath = 'https://historic-arches-33577.herokuapp.com';
+  backendPath = "https://historic-arches-33577.herokuapp.com";
 }
 
 export const API_ROOT = backendPath;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    '/api/*',
+    "/api/*",
     proxy({
-      target: 'http://localhost:5000',
+      target: "http://localhost:8888",
     })
   );
 };
